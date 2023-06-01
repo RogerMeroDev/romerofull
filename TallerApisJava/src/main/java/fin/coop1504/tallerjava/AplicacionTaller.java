52b4e74db0fc819690d9a7bc8b83d9f9f7b3216b
Rogelio <romero@coop15abril.fin.ec>
2023-06-01 12:01:56 -0500

    excepciones

---
+++ TallerApisJava/src/main/java/fin/coop1504/tallerjava/AplicacionTaller.java
@@ -0,0 +1,81 @@
+package fin.coop1504.tallerjava;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+public class AplicacionTaller {
+
+	public static void main(String[] args) {
+		String cadena="Hola Mundo";
+		System.out.println(cadena.concat("Tierra"));
+		System.out.println(cadena);
+		StringBuffer cadenaEspecial=new StringBuffer("Hola MUNDO");
+		cadenaEspecial.append("Tierra");
+		System.out.println(cadenaEspecial.toString());
+		
+		for(int i=cadena.length()-1;i>=0;i--) {
+			System.out.print(cadena.charAt(i));
+			
+		}
+		/*for resumido*/
+		
+		for(char a:cadena.toCharArray()) {
+			System.out.println(a);
+		}
+		
+		System.out.println(cadenaEspecial.reverse());
+		
+		/*Arreglos*/		
+		String[] cadenas=new String[4];
+		cadenas[0]="Karen";
+		cadenas[1]="Pepito";
+		cadenas[2]="Maria";
+		cadenas[3]="Juanito";
+		/*Recorrer el arreglo anterior*/
+		for(String cad:cadenas) {
+			System.out.println(cad);
+		}
+		
+		/*Metodo2*/
+		String[] cadenas2 = {"1","2","3","4"};
+		for(String cad:cadenas2) {
+			System.out.println(cad);
+		}
+		
+		/*Vector*/
+		
+		Vector<String> vector =new Vector();
+		vector.add("Cad1");
+		vector.add("Cad2");
+		vector.add("Cad3");
+		
+		System.out.println(vector.size());
+		
+		/*ArrayList*/
+		
+		ArrayList<Integer> lista= new ArrayList<Integer>();
+		lista.add(10);
+		lista.add(20);
+		lista.add(30);
+		lista.add(40);
+		
+		System.out.println(lista.size());
+		
+		lista.forEach(elemento->{
+			if(elemento.equals(20))
+				System.out.println(elemento + 20);
+			else
+				System.out.println(elemento);
+		});
+		/*Imprimer dato de un vector se crea nuevo objeto*/
+		
+		String cadenaVector =vector.get(0);
+		System.out.println(cadenaVector);
+		
+		/*Se envia el mismo objeto sin crearlo*/
+		Integer numeroArrayList= lista.get(0);
+		System.out.println(numeroArrayList);
+
+	}
+
+}
--- taller-controluniversal/src/main/java/fin/coop1504/tallerjava/AplicacionTaller.java
+++ taller-controluniversal/src/main/java/fin/coop1504/tallerjava/AplicacionTaller.java
@@ -3,6 +3,7 @@ package fin.coop1504.tallerjava;
 import fin.coop1504.tallerjava.entidades.AireAcondicionado;
 import fin.coop1504.tallerjava.entidades.ControlUniversal;
 import fin.coop1504.tallerjava.entidades.Televisor;
+import fin.coop1504.tallerjava.interfaces.ReferenciaDispositivo;
 
 public class AplicacionTaller {
 
@@ -14,6 +15,21 @@ public class AplicacionTaller {
 		ControlUniversal.encenderDispositivo(tvSala);
 		ControlUniversal.encenderDispositivo(aireacondicionado);
 		
+		/*CLASE ANONIMA SIN CREAR OTRA CLASE*/
+		ControlUniversal.encenderDispositivo(new ReferenciaDispositivo() {
+			
+			@Override
+			public void encender() {
+				System.out.println("Encender Dispositivo DE CLASE ANONIMA");
+				
+			}
+			
+			@Override
+			public void apagar() {
+				System.out.println("Apagar de CLASE ANONIMA");
+				
+			}
+		});
 	}
 
 }
